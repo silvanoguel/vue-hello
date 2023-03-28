d2ccc0423b33b008c9cd26f8b85330f9a8b06097
silvanoguel <silvanoguel@msn.com>
2023-03-28 13:27:48 +0200

    add:project completed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
 // Descrizione:
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus:
-//Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 
 const {createApp} = Vue;
 
 createApp({
     data(){
         return {
-            
-
+            message: "Hello user",
+            image: "https://d585tldpucybw.cloudfront.net/sfimages/default-source/default-album/870x220_telerik-blogsce2fdf065dfe46a2b2054ccc57d2d18d.png?sfvrsn=222ff053_1",
         }
     },
-    methods: {
-        
-    }
+
 }).mount("#app")
